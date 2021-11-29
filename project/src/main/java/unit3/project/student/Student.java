5ff2bfec0b7ec2aa05e67d512b7e70797680967c
Nouraoqail <Nouraoqail.10@gmail.com>
2021-11-29 22:07:47 +0300

    Student package

--- project/src/main/java/unit3/project/student/Student.java
+++ project/src/main/java/unit3/project/student/Student.java
@@ -1,4 +1,78 @@
 package unit3.project.student;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Student")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String fName;
+    private String lName;
+    private String email;
+    private String gender;
+
+    public Student(long id, String fName, String lName, String email, String gender) {
+        this.id = id;
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public Student(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
 }
--- project/src/main/java/unit3/project/student/StudentController.java
+++ project/src/main/java/unit3/project/student/StudentController.java
@@ -1,4 +1,44 @@
 package unit3.project.student;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="students")
 public class StudentController {
+
+    private final StudentService StudentService;
+
+    @Autowired
+    public StudentController(StudentService StudentService) {
+
+        this.StudentService = StudentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudent(){
+        return StudentService.getStudent();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudents(@PathVariable String id){
+        return StudentService.getStudents(id);
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student){
+        return StudentService.createStudent(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable String id){
+        StudentService.deleteStudent(id);
+    }
+    @PutMapping("/{id}")
+    public void updateStudent(@PathVariable String id, @RequestBody Student data){
+        StudentService.updateStudent(id, data);
+    }
 }
