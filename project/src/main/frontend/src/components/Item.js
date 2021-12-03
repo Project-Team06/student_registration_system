d7f7ebe15eb152d43658f621c2801deb001daf6b
Shrooq713 <shroog713@gmail.com>
2021-12-03 14:10:40 +0300

    “table”

---
+++ project/src/main/frontend/src/components/Item.js
@@ -0,0 +1,16 @@
+function Item({ course }) {
+  return (
+    <div className="align">
+      {/* course box */}
+      <div className="cd-timeline-content cd-right cd-container design">
+        <h4>{course.courseName}</h4>
+        Time: {course.startTime} - {course.endTime}
+        <br />
+        room id: {course.roomID}
+        <br />
+        final Exam Date: {course.roomID}
+      </div>
+    </div>
+  );
+}
+export default Item;
